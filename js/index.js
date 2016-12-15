749618497b5e3099b4ef4aad7935142138977203
Roman Khoronzhak <romix.price.3xd@gmail.com>
2016-12-15 16:19:08 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-// var n = Number(prompt('Enter n:'));
-
-// function fibo(n) {
-//   return n <= 1 ? n : fibo(n - 1) + fibo(n - 2);
-// }
-
-// document.write(fibo(n));
-
 var n = Number(prompt('Enter n:'));
 
 function fibo(n) {
